f5f6cca28a21f861717caebb26b882e56a28772e
Fundosh1 <vividblue74@gmail.com>
2026-04-20 21:07:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -246,7 +246,6 @@ app.all('*', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
   console.log(`OpenAI to NVIDIA NIM Proxy running on port ${PORT}`);
   console.log(`Health check: http://localhost:${PORT}/health`);
   console.log(`Reasoning display: ${SHOW_REASONING ? 'ENABLED' : 'DISABLED'}`);
